f5cfd52239076fe2109834700c82e2539bbc30ec
DonSintaro <jarriaga.important@gmail.com>
2019-10-21 18:52:35 -0500

    Started ground work and created arrays/html

---
+++ PasswordGen.jsx
@@ -0,0 +1,25 @@
+var arrayLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var arrayUpper = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"];
+var arrayNums = [1,2,3,4,5,6,7,8,9,0];
+var arraySpecials = ["!","@","#","$","%","^","&","*","(",")","-","_","=","+"," ","'",'"',";",":",".","[","{","]","}","\\","|",",","<",".",">","/","?","`","~"];
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var parsed = parseInt(input);
+if (isNaN(parsed) || parsed.toString().length != input.length) { 
+  return "failure" 
+}
+return "success";
+}
+
+*/
