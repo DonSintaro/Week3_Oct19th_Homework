624eb63326a90a199566f1a84ecf09e076fb59b1
DonSintaro <jarriaga.important@gmail.com>
2019-10-21 22:56:31 -0500

    Added more functions to js file

--- PasswordGen.jsx
+++ PasswordGen.jsx
@@ -4,6 +4,20 @@ var arrayNums = [1,2,3,4,5,6,7,8,9,0];
 var arraySpecials = ["!","@","#","$","%","^","&","*","(",")","-","_","=","+"," ","'",'"',";",":",".","[","{","]","}","\\","|",",","<",".",">","/","?","`","~"];
 
 
+var validBool = false;
+var userInputBool;
+var userInputNum;
+
+////Options for desired password////
+var specialOpt;
+var numbersOpt;
+var upperOpt;
+var lowerOpt;
+
+
+
+
+var randomNum = Math.floor((Math.random() * userLimit)+1);
 
 
 
@@ -13,13 +27,42 @@ var arraySpecials = ["!","@","#","$","%","^","&","*","(",")","-","_","=","+"," "
 
 
 
+/////////////////Validation of numvber///////////////////////////
 
-/*
-var parsed = parseInt(input);
-if (isNaN(parsed) || parsed.toString().length != input.length) { 
-  return "failure" 
+function validateNum(x){
+var parsed = parseInt(x);
+if (isNaN(parsed) || parsed.toString().length != x.length) { 
+  return false;
 }
-return "success";
+return true;
 }
 
-*/
+
+//////////////Validation of Limit////////////////////////////////
+function validateLimit(userInputNum){
+  do { 
+
+    userInputNum = prompt("Choose a length number between 8 and 128 characters: ");
+    validBool = validateNum(userInputNum);
+
+    if ((validBool == false) || (userInputNum > 128) || (userInputNum < 8)) {
+      alert("That is not a proper input, please try again..");  
+    }
+  }
+  while ((validBool == false) || (userInputNum > 128) || (userInputNum < 8));
+}
+
+///////////General use convert Y/N to true/false/null////////////
+function convertToBool(x){
+  if (x.toUpperCase() === "Y"){
+    return true;
+  }
+  else if (x.toUpperCase() === "N"){
+    return false;
+  }
+  else{
+    return null;
+  }
+}
+
+/// need to create series of if statements to determine options in password creation, example: special y/n, numbers y/n
